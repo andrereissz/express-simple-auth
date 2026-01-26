618cbf94b7be6878acadcd7e611aeb69c89f9ee2
André Reis <101909552+andrereissz@users.noreply.github.com>
2026-01-26 18:58:10 -0300

    feat: adds validation

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import validator from 'express-validator';
 import * as fakedb from './fakedb.js';
 import * as hash from './utils/hash.js';
 const app = express();
@@ -10,34 +11,59 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
-app.post('/login', async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+const loginChain = [
+    validator.body('username')
+        .notEmpty(),
+
+    validator.body('password')
+        .notEmpty()
+];
+
+const registerChain = [
+    validator.body('username')
+        .notEmpty()
+        .escape(),
+
+    validator.body('password')
+        .notEmpty()
+];
+
+app.post('/login', loginChain, async (req, res) => {
+    const result = validator.validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).send({ errors: result.array() });
+    }
+
+    const { username, password } = req.body;
 
     const user = fakedb.findUserByUsername(username);
 
-    if(!user) {
-        res.status(404);
+    if (!user) {
+        res.status(404).send('User not Found');
     }
 
     const compare = await hash.comparePassword(password, user.password);
 
-    res.json({ login: compare });
+    return res.json({ login: compare });
 });
 
-app.post('/register', async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+app.post('/register', registerChain, async (req, res) => {
+    const result = validator.validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).send({ errors: result.array() });
+    }
+
+    const { username, password } = req.body;
 
     const user = fakedb.findUserByUsername(username);
 
-    if(user) {
-        res.status(403).send("Username has already been taken.");
+    if (user) {
+        return res.status(409).send("Username has already been taken.");
     }
 
     const hashedPassword = await hash.hashPassword(password);
 
     fakedb.createUser(username, hashedPassword);
 
-    res.json({ message: "User created successfully", users: fakedb.db.users });
+    return res.json({ message: "User created successfully", users: fakedb.db.users });
 });
