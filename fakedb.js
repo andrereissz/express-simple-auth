597dbc616745fc4b40f73791d9fa476dad82ec00
André Reis <101909552+andrereissz@users.noreply.github.com>
2026-01-27 16:20:15 -0300

    refactor: updates the database to handle UUIDs

--- fakedb.js
+++ fakedb.js
@@ -1,10 +1,12 @@
 export let db = {
     users: [
         {
+            id: "e2a15d35-3870-4b54-9ace-ba35f65e8d0e",
             username: "user1",
             password: "$2a$10$I3qJNfyMhJFI4kTr3jlysuodr3CdOyLTkVIEEH1fyXWDbPOgLm9X2"
         },
         {
+            id: "4108bd97-c3fd-482c-a2f6-31c144339e0a",
             username: "user2",
             password: "$2a$10$I3qJNfyMhJFI4kTr3jlysuodr3CdOyLTkVIEEH1fyXWDbPOgLm9X2"
         }
@@ -18,5 +20,6 @@ export function findUserByUsername(username) {
 }
 
 export function createUser(username, password) {
-    db.users.push({ username: username, password: password });
+    const uuid = crypto.randomUUID();
+    db.users.push({ id: uuid, username: username, password: password });
 }
