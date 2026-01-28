b692893503f05912cd41304578bfd70c35fc758d
André Reis <101909552+andrereissz@users.noreply.github.com>
2026-01-28 00:44:18 -0300

    refactor: organizes the AuthController and adds a new method that handles the AccessToken regeneration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
 import * as hash from '../utils/hash.js';
+
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
@@ -8,54 +9,82 @@ const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 class AuthController {
 
     login = async (req, res) => {
-
         const { username, password } = req.body;
-
         const user = User.findByUsername(username);
-
         if (!user) {
             return res.status(404).send('User not Found');
         }
 
         const compare = await hash.comparePassword(password, user.password);
-
         if (!compare) {
             return res.status(401).send('Invalid Credentials');
         }
 
-        const accessToken = jwt.sign({ userId: user.id, username: user.username }, ACCESS_TOKEN_SECRET, { expiresIn: '10s' });
-        const refreshToken = jwt.sign({ userId: user.id }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
-
+        const accessToken = this.generateAccessToken(user.id, user.username);
+        const refreshToken = this.generateRefreshToken(user.id);
         User.storeRefreshToken(user.id, refreshToken);
 
-        res.cookie('access_token', accessToken, {
+        res.cookie('accessToken', accessToken, {
+            httpOnly: true,
+            sameSite: true
+        });
+        res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             sameSite: true
         });
 
-        res.cookie('refresh_token', refreshToken, {
-            httpOnly: true,
-            sameSite: true
-        });
-
-        return res.status(200).json({ access_token: accessToken, refresh_token: refreshToken, users: User.db.users });
+        return res.status(200).json({ accessToken: accessToken, refreshToken: refreshToken });
     }
 
     register = async (req, res) => {
-
         const { username, password } = req.body;
-
         const user = User.findByUsername(username);
-
         if (user) {
             return res.status(409).send("Username has already been taken.");
         }
 
         const hashedPassword = await hash.hashPassword(password);
-
         User.create(username, hashedPassword);
 
-        return res.json({ message: "User created successfully", users: User.db.users });
+        return res.json({ message: "User created successfully" });
+    }
+
+    generateAccessToken = (userId, username) => {
+        const token = jwt.sign({ userId, username }, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+
+        return token;
+    }
+
+    generateRefreshToken = (userId) => {
+        const token = jwt.sign({ userId }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+
+        return token;
+    }
+
+    refreshToken = async (req, res) => {
+        try {
+            const refreshToken = req.cookies.refreshToken;
+            if (!refreshToken) {
+                throw new Error("Refresh Token not Found.");
+            }
+
+            const payload = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
+
+            const user = User.find(payload.userId);
+            if (!user) {
+                throw new Error("User not Found.");
+            }
+
+            const accessToken = this.generateAccessToken(user.id, user.username);
+
+            return res.cookie('accessToken', accessToken, {
+                httpOnly: true,
+                sameSite: true
+            });
+
+        } catch (error) {
+            return res.status(403).json({ error: error.message });
+        }
     }
 }
 
